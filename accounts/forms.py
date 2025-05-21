e25655c799a770d86d2c583d786de352b413928f
Ankush <ap7858025@gmail.com>
2025-05-21 13:54:14 +0530

    Made all registration fields required, improved login logic, and added profile images

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,23 +1,3 @@
-# from django import forms
-# from django.contrib.auth.forms import UserCreationForm
-# from .models import CustomUser
-
-# class CustomUserCreationForm(UserCreationForm):
-#     password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-#     password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
-
-#     class Meta:
-#         model = CustomUser
-#         fields = [
-#             'first_name', 'last_name', 'username', 'email',
-#             'user_type', 'profile_picture',
-#             'address_line1', 'city', 'state', 'pincode',
-#             'password1', 'password2'
-#         ]
-
-
-
-
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
@@ -47,7 +27,7 @@ class CustomUserCreationForm(UserCreationForm):
             'password1', 'password2'
         ]
         help_texts = {
-            'username': None,  # remove default help text
+            'username': None,
         }
         widgets = {
             'first_name': forms.TextInput(attrs={
@@ -89,3 +69,16 @@ class CustomUserCreationForm(UserCreationForm):
                 'placeholder': 'Pincode'
             }),
         }
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        # Make all fields required
+        for field_name in self.fields:
+            self.fields[field_name].required = True
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords do not match.")
+        return password2
--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,11 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 
 
 def home_view(request):
@@ -26,9 +31,16 @@ def register_view(request):
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user_exists = User.objects.filter(username=username).exists()
+
+        if not user_exists:
+            messages.error(request, "Invalid username.")
+        elif not form.is_valid():
+            messages.error(request, "Invalid password.")
+        else:
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
@@ -36,8 +48,12 @@ def login_view(request):
                     return redirect('patient_dashboard')
                 elif user.user_type == 'doctor':
                     return redirect('doctor_dashboard')
+            else:
+                messages.error(request, "Invalid username or password.")
+
     else:
         form = AuthenticationForm()
+    
     return render(request, 'accounts/login.html', {'form': form})
 
 
